b6cd3fe64e9e5f565efc5502f998ebc335cf4549
unknown <sdaros@SQL70135-5.SQLI>
2016-11-17 14:34:12 +0100

    View list

--- src/main/resources/static/todo.js
+++ src/main/resources/static/todo.js
@@ -2,36 +2,56 @@ $(document).ready(function() {
     $.ajax({
         url: "/todos"
     }).then(function(data) {
-       $('.todos').append(getLine(data[0]));
+       for (var item in data) {
+        $('.todos').append(getLineItem(data[item]));
+       }
+    }).fail(function() {
+        //$('.alert-danger').show();
     });
+    $('.alert').hide();
+    $('#create-button').addClass('disabled');
+
+
+
+       $('#todo-text').on('change keyup paste', function() {
+            if($('#todo-text').val().length > 0) {
+                $('#create-button').removeClass('disabled');
+            } else {
+                $('#create-button').addClass('disabled');
+            }
+
+       });
 
      $('#create-button').click(function() {
-         var data = {};
-         data["texte"] = $('#todo-test').val();
-         $.ajax({
-            type: "POST",
-            url: "/todos",
-            contentType: 'application/json',
-            data : JSON.stringify(data),
-            dataType: 'json'
-         }).then(function(data) {
-            $('.todos').prepend(getLine(data[0]));
-         });
-     });
+        if(!$('#create-button').hasClass( "disabled" )) {
+                 var dataSend = {};
+                 dataSend["texte"] = $('#todo-text').val();
+                 $.ajax({
+                    type: "POST",
+                    url: "/todos",
+                    contentType: 'application/json',
+                    data : JSON.stringify(dataSend),
+                    dataType: 'json'
+                 }).then(function(data) {
+                    $('.todos').prepend(getLine(data.id,dataSend.texte,'ACTIVE'));
+                    $('.alert-success').show();
+                 }).fail(function() {
+                       $('.alert-danger').show();
+                   });
+          }
+        });
 
-    function getLine(item) {
-        var template = '<li class="list-group-item">'+
-                '<span class="todos_id"><strong>'+item.id+'</strong> - </span>'+
-                '<span class="todos_text">'+item.texte+'</span>'+
-                '<span class="todos_status badge">'+item.status+'</span>'+
-            '</li>';
-
-        return template;
+    function getLineItem(item) {
+        return getLine(item.id,item.texte,item.status);
     }
 
-    function createItemJson(texte) {
-        var itemJson = 'texte:"'+texte+'"';
-        return JSON.stringify(itemJson);
+    function getLine(id,texte,status) {
+        var template = '<li class="list-group-item">'+
+                '<span class="todos_id hidden"><strong>'+id+'</strong></span>'+
+                '<span class="todos_text">'+texte+'</span>'+
+                '<span class="todos_status badge">'+status+'</span>'+
+            '</li>';
+        return template;
     }
 
 });
