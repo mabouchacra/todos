a3a3228e7c4e6540c24cafea7eb2c84a639df430
unknown <sdaros@SQL70135-5.SQLI>
2016-11-17 11:48:35 +0100

    CREATE and GET items - Front

--- src/main/resources/static/todo.js
+++ src/main/resources/static/todo.js
@@ -2,7 +2,36 @@ $(document).ready(function() {
     $.ajax({
         url: "/todos"
     }).then(function(data) {
-       $('.todos').append(data[0].responsable);
-        console.log(data[0].responsable);
+       $('.todos').append(getLine(data[0]));
     });
+
+     $('#create-button').click(function() {
+         $.ajax({
+            type: "POST",
+            url: "/todos",
+            data: createItemJson($('#todo-test').val()),
+         }).then(function(data) {
+            $('.todos').prepend(getLine(data[0]));
+         });
+     });
+
+    function getLine(item) {
+        var template = '<li class="list-group-item">'+
+                '<span class="todos_id"><strong>'+item.id+'</strong> - </span>'+
+                '<span class="todos_text">'+item.texte+'</span>'+
+                '<span class="todos_status badge">'+item.status+'</span>'+
+            '</li>';
+
+        return template;
+    }
+
+    function createItemJson(texte) {
+        var itemJson = '[{"id":"null",' +
+            '"texte":"'+texte+'",'+
+            '"responsable":"Mounir",'+
+            '"status":"ACTIVE"}]'
+
+            return itemJson;
+    }
+
 });
